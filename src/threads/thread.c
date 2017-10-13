ff5718e88606e889144bbe850ebeac30f61a4091
SangHyeon Lee <leesh6796@gmail.com>
2017-10-13 21:16:12 +0900

    [PJ2] Add initialization of t->files

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -594,6 +594,7 @@ init_thread (struct thread *t, const char *name, int priority)
   list_init (&t->acquired_locks);
   list_init (&t->lost_locks);
   list_init (&t->children);
+  list_init (&t->files);
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
